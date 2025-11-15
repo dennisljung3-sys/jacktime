1c6d0d9824914245224c6260d300a889bf09c749
dennisljung3-sys <dennis.ljung3@gmail.com>
2025-11-15 10:55:27 +0100

    uppdatera readme

---
+++ tillfälg_kamera.py
@@ -0,0 +1,17 @@
+import cv2
+
+cap = cv2.VideoCapture(0)  # prova även 1 om 0 inte funkar
+if not cap.isOpened():
+    print("❌ Kunde inte öppna kameran")
+else:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("❌ Ingen bild från kameran")
+            break
+        cv2.imshow("Testkamera", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
